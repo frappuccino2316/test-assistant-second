1e2069ce3903069496bcd795771ad27638ae4600
frappuccino2316 <frappuccino2316@gmail.com>
2024-04-02 00:27:22 +0900

    各ボタンに生成イベントを紐づけ

--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,22 @@
-import { generate100Text } from './text/generate';
+import {
+  generate100Text,
+  generate255Text,
+  generate500Text,
+  generate1000Text,
+  generate2000Text,
+} from './text/generate';
 
 const btn100 = document.getElementById('generate100');
 btn100!.addEventListener('click', generate100Text);
+
+const btn255 = document.getElementById('generate255');
+btn255!.addEventListener('click', generate255Text);
+
+const btn500 = document.getElementById('generate500');
+btn500!.addEventListener('click', generate500Text);
+
+const btn1000 = document.getElementById('generate1000');
+btn1000!.addEventListener('click', generate1000Text);
+
+const btn2000 = document.getElementById('generate2000');
+btn2000!.addEventListener('click', generate2000Text);
